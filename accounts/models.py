8d3426b105ef347bc893278895026fa97a6c40de
Vaishnavi Kamthe <vaishnavikamthe1884@gmail.com>
2025-07-22 20:27:56 +0530

    created accounts model, upated urls. Temporary view(add ui)

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Custom user model
+class User(AbstractUser):
+    is_student = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+    
+# Student profile data
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    degree = models.CharField(max_length=100)
+    passing_year = models.IntegerField()
+    skills = models.TextField(help_text="Comma-separated skills")
+    resume = models.FileField(upload_to='resumes/', null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
