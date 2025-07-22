e88004117fcf1da2c02c318fb7fa3f7676f8b95e
Vaishnavi Kamthe <vaishnavikamthe1884@gmail.com>
2025-07-22 12:50:23 +0530

    Created Django apps and registered in settings.py

--- tnp_portal/settings.py
+++ tnp_portal/settings.py
@@ -31,12 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my apps
+    'accounts',
+    'jobs',
+    'applications',
+    'dashboard',
+    'notifications',
+    'classifier',
 ]
 
 MIDDLEWARE = [
